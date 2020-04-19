0b9339b5d94db445812fbc0335657268e61846be
jalberto <jalberto.coder@gmail.com>
2020-04-19 13:16:04 +0200

    Add solarized theme to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -3,8 +3,6 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
 (menu-bar-mode 1)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
@@ -20,11 +18,21 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (which-key-mode 1)
 
+(elpy-enable)
+
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
     ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default)))
- '(package-selected-packages (quote (solarized-theme which-key smex powerline))))
+ '(package-selected-packages (quote (org elpy solarized-theme which-key smex powerline))))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
